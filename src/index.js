32b2e53f65814d95a7ea65129916c036a1b510a4
oxonomy14 <oxonomy14@gmail.com>
2026-03-10 23:38:55 +0700

    install express

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
-const message = 'This is Epharmacy admin backend';
-console.log(message);
+import express from 'express';
+
+
+
+const PORT = 3000;
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json ({
+    message: "Backend Server Admin E-pharmacy is running",
+});
+});
+
+app.listen (PORT, () => {
+console.log(`Backend Server Admin E-pharmacy is running on port ${PORT}`);
+
+
+});
